7b28cfbea019eed8b6316bf1ed47d338d3d7ce24
Fredy Orozco <freorozcoloa@gmail.com>
2022-09-25 22:29:26 -0500

    Creating iris

---
+++ iris_model.py
@@ -0,0 +1,34 @@
+#importar las librerias necesarias
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
+import pickle
+import os
+
+#cargar los datos en un dataset
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+#separar los datos en entrenamiento y prueba
+x_train, x_test, y_train, y_test = train_test_split(X, y)
+
+lin_reg = LinearRegression()
+log_reg = LogisticRegression()
+svc_m = SVC()
+
+#entrenar modelos
+lin_regr = lin_reg.fit(x_train, y_train)
+log_regr = log_reg.fit(x_train, y_train)
+svc_mo = svc_m.fit(x_train, y_train)
+
+with open(os.path.join('model','lin_reg.pkl'), 'wb') as li:
+    pickle.dump(lin_regr, li)
+
+with open(os.path.join("model",'log_reg.pkl'), 'wb') as lo:
+    pickle.dump(log_regr, lo)
+
+with open(os.path.join("model",'svc_m.pkl'), 'wb') as sv:
+    pickle.dump(svc_mo, sv)
