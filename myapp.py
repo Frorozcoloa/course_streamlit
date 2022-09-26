3b9feb814287b5311f2573acedf8d73ad64b91e8
Fredy Orozco <freorozcoloa@gmail.com>
2022-09-25 21:59:17 -0500

    Does a simple aplication

--- myapp.py
+++ myapp.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# Simple Stock Price App
-Shown are the stock closing price and volume of Google!
+## Simple Stock Price App
+Shown are the stock **closing price** and **volume** of Google!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
@@ -16,5 +16,11 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing price
+         """)
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume price
+         """)
 st.line_chart(tickerDf.Volume)
